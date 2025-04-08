4381c05d57dbf3f3025ace9a59e79e42d2a853b2
pediland <pediland.com@gmail.com>
2025-04-08 17:01:53 +0200

    "Day 2 of Mini Project"

--- src/quiz.js
+++ src/quiz.js
@@ -1,42 +1,59 @@
 class Quiz {
-    constructor (questions, timeLimit, timeRemaining){
-        this.questions = questions;
-        this.timeLimit = timeLimit;
-        this.timeRemaining = timeRemaining;
-        this.correctAnswers = 0;
-        this.currentQuestionIndex = 0;
-    }
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    getQuestion(){
-        return this.questions[this.currentQuestionIndex]
-    }
-    
-     moveToNextQuestion(){
-        return this.currentQuestionIndex++;
-     }
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    shuffleQuestions(){
-        for (let i = this.questions.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
-        }
-        return this.questions;
+  moveToNextQuestion() {
+    return this.currentQuestionIndex++;
+  }
 
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
     }
+    return this.questions;
+  }
 
-    checkAnswer(answer){
-        if (this.answer !== 0){
-            this.correctAnswers++;
-        }
-        
+  checkAnswer(answer) {
+    if (this.answer !== 0) {
+      this.correctAnswers++;
     }
+  }
 
-    hasEnded(){
-        if (this.currentQuestionIndex < this.questions.length){
-            return false;
-        }
-        if (this.currentQuestionIndex == this.questions.length) {
-            return true;
-        }
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
     }
+    if (this.currentQuestionIndex == this.questions.length) {
+      return true;
+    }
+  }
+
+  filterQuestionsByDifficulty(difficulty) {
+    if (typeof difficulty === "number" && difficulty >= 1 && difficulty <= 3) {
+      this.questions = this.questions.filter(
+        (question) => question.difficulty === difficulty
+      );
+    }
+  }
+
+  averageDifficulty() {
+    const totalDifficulty = this.questions.reduce(
+      (total, question) => total + question.difficulty,
+      0
+    );
+    return totalDifficulty / this.questions.length;
+  }
 }
